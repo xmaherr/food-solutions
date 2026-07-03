29b0e4db42da1671b2050d9a828aaaab6bf500c2
xmaherr <mahernabil219@gmail.com>
2026-07-03 19:29:41 +0300

    feat: add branding configuration file and remove it from gitignore tracking

---
+++ config/branding.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'app_name' => 'Food Solutions',
+    'primary_color' => '#14594F',
+    'secondary_color' => '#E69D65',
+    'text_color' => '#FFF4E2',
+    'surface_color' => '#f0ede8',
+    'accent_color' => '#f0f9f8',
+    'nav_hover_color' => 'rgba(255,255,255,0.15)',
+];
